e7c5c5723dcd6b68cba71d3dbeeeda6fea3a8325
Paulina Siwko <pau.siwko@gmail.com>
2024-01-05 15:32:42 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Modal
+// const myModal = document.getElementById('#myModal')
+// const myInput = document.getElementById('#myInput')
+
+// myModal.addEventListener('shown.bs.modal', () => {
+//   myInput.focus()
+// })
+
+
+// Time
+var currentTime = dayjs();
+$("#current-time").text(currentTime.format("DD MMM YYYY, HH:mm:ss a"));
